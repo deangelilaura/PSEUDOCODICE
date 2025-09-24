0d301acc40f90e1646358a9d8dc5e863eb3e08c0
laura <deangeli.laura@iisgalvanimi.edu.it>
2025-09-24 08:41:11 +0000

    da finire

---
+++ giorno4/min max array.py
@@ -0,0 +1,4 @@
+array = [4, 7, 8, 3, 5]
+array.sort()
+print("minimi: ", array[0])
+print("massimi: ", array[-1])
---
+++ giorno4/n dato in un array.py
@@ -0,0 +1,4 @@
+array = [1, 7, 8, 3, 5, 7, 1]
+n = (int(input("inserisci un numero da cercare nell'array: ")))
+count = array.count(n)
+print(f"in numero {n} appare {count} volte nell'array")
